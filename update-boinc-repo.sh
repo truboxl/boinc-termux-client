f3caa08a5bf94e772b7e3c1d27b406a4fe71a1cd
Jia Yuan Lo <jylo06g@gmail.com>
2022-06-20 10:55:38 +0800

    update patches

--- update-boinc-repo.sh
+++ update-boinc-repo.sh
@@ -2,14 +2,24 @@
 set -e
 export REPO="${PWD}"
 if [ ! -d ./src/boinc*/ ]; then
-    echo "Cloning repo"; git clone https://github.com/boinc/boinc src/boinc
+	echo "git clone https://github.com/boinc/boinc src/boinc"
+	git clone https://github.com/boinc/boinc src/boinc
 fi
-echo "Entering repo"; cd ./src/boinc*/
-echo "git fetch"; git fetch
-echo "git reset"; git reset --hard origin/master
-echo "git clean"; git clean -fxdq
-echo "git apply"
-git apply ${REPO}/patches/boinc-termux-patches.patch
-#git apply ${REPO}/patches/boinc-termux-patches-app_start-api23.patch
-git apply ${REPO}/patches/boinc-domain-name.patch
+
+echo "cd ./src/boinc*/"
+cd ./src/boinc*/
+
+echo "git fetch"
+git fetch
+
+echo "git reset --hard origin/master"
+git reset --hard origin/master
+
+echo "git clean -fxdq"
+git clean -fxdq
+
+echo "patch -p1 -i *.patch"
+for patch in ${REPO}/patches/*.patch; do
+	patch -p1 -i "$patch"
+done
 echo "Done"
